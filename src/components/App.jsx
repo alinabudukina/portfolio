b3f974a853118ec9f1e3b2ed1e3af5cbb121f9f0
alinabudukina <alinabudukina@gmail.com>
2021-06-25 12:02:30 +0200

    new component for cursor created because the react lib didn't work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import About from './About/About';
 import Projects from './Projects/Projects';
 import Contact from './Contact/Contact';
 import Footer from './Footer/Footer';
-import AnimatedCursor from 'react-animated-cursor';
+import Cursor from './Cursor/Cursor';
 
 import { PortfolioProvider } from '../context/context';
 
@@ -16,6 +16,7 @@ function App() {
   const [projects, setProjects] = useState([]);
   const [contact, setContact] = useState({});
   const [footer, setFooter] = useState({});
+  //const [cursor, setCursor] = useState({});
 
   useEffect(() => {
     setHero({ ...heroData });
@@ -26,21 +27,8 @@ function App() {
   }, []);
 
   return (
-    // https://www.digitalocean.com/community/tutorials/how-to-create-custom-components-in-react-fr
-    // !! https://github.com/stephenscaff/react-animated-cursor
-    // 1111 https://www.npmjs.com/package/react-animated-cursor
-
-    // https://www.youtube.com/watch?v=1ECEdO7zHMY
-    // https://www.reddit.com/r/learnjavascript/comments/i97jn2/problem_jsx_expressions_must_have_one_parent/
     <PortfolioProvider value={{ hero, about, projects, contact, footer }}>
-      <AnimatedCursor
-        innerSize={8}
-        outerSize={8}
-        color="193, 11, 111"
-        outerAlpha={0.2}
-        innerScale={0.7}
-        outerScale={5}
-      />
+      <Cursor />
       <Hero />
       <About />
       <Projects />
---
+++ src/components/Cursor/Cursor.jsx
@@ -0,0 +1,7 @@
+import React, { useContext } from 'react';
+
+const Cursor = () => {
+  return <h1></h1>;
+};
+
+export default Cursor;
