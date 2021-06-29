ccf3d2da29dbbf89e3f9c4722d389f909fe802cd
alinabudukina <alinabudukina@gmail.com>
2021-06-29 13:52:20 +0200

    cursor deleted

--- src/components/Cursor/Cursor.jsx
+++ src/components/Cursor/Cursor.jsx
@@ -1,20 +1,7 @@
 import React from 'react';
-import AnimatedCursor from 'react-animated-cursor';
 
 const Cursor = () => {
-  //return <h1> Coucou</h1>;
-  return (
-    <div className="cursor">
-      <AnimatedCursor
-        innerSize={8}
-        outerSize={8}
-        color="193, 11, 111"
-        outerAlpha={0.2}
-        innerScale={0.7}
-        outerScale={5}
-      />
-    </div>
-  );
+  return <h1></h1>;
 };
 
 export default Cursor;
