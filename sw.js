c6a7b62854488d5535a17d45a2763a6752a03a40
alinabudukina <alinabudukina@gmail.com>
2021-06-29 16:37:34 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-141c083f56fa754ebcce.js"
+    "url": "webpack-runtime-9259a6b2b2457d674c2c.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6e99a0a8253f43bb1709972611304a6d"
+    "revision": "70adec49af7b0a96a5b2b6370a17ea73"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "79f1882d933f5bbb23074efa06ca575a"
+    "revision": "b7d1776194b23b3feb1646e561edd5dd"
   },
   {
     "url": "polyfill-57fa76c8ad53bd44c5a1.js"
